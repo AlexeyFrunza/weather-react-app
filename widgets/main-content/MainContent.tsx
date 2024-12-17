ca624124c53865abdaf0245ad130caeb96b06eb2
AlexeyFrunza <tvoih8r@mail.ru>
2024-12-17 22:18:56 +0200

    initial state

---
+++ widgets/main-content/MainContent.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./MainContent.module.scss";
+
+function MainContent() {
+  return (
+    <>
+      <div className={styles.main__container}></div>
+    </>
+  );
+}
+
+export default MainContent;
